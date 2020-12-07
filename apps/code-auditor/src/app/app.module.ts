53805bc8107e01741b73bb7fddd449d02a35b7d9
Kzoeps <kzoepa@gmail.com>
2020-12-07 13:14:56 +0600

    feat: added angular material to the app

--- apps/code-auditor/src/app/app.module.ts
+++ apps/code-auditor/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, NoopAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
