417263ea9f54274b5d491db6ccbf02052f91b534
Kzoeps <kzoepa@gmail.com>
2020-12-07 12:46:48 +0600

    feat(nx): created custom workspaceLayout to automatically create libraries in feature dir

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/code-auditor'],
+  projects: [
+    '<rootDir>/apps/code-auditor',
+    '<rootDir>/libs/test',
+    '<rootDir>/libs/feature/test',
+  ],
 };
