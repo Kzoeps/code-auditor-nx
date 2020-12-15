64e2808db20ec7dc9f862ae00e54c04df0fa2678
Kzoeps <kzoepa@gmail.com>
2020-12-15 19:04:30 +0600

    feat(audit-form): made the form creator dynamic

---
+++ libs/feature/audit/src/lib/components/edit-audit/edit-audit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'selise-start-edit-audit',
+  templateUrl: './edit-audit.component.html',
+  styleUrls: ['./edit-audit.component.css']
+})
+export class EditAuditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
