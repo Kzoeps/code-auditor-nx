518da73dd59262bf8907fcd053f24accbebe6f1c
Kzoeps <kzoepa@gmail.com>
2020-12-15 13:52:51 +0600

    feat(audits.component): shows a list of all audits and links them respectively

--- libs/feature/audit/src/lib/feature-audit.module.ts
+++ libs/feature/audit/src/lib/feature-audit.module.ts
@@ -9,9 +9,11 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatDatepickerModule, MatButtonModule, ReactiveFormsModule, MatCardModule],
+  imports: [CommonModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatDatepickerModule, MatButtonModule, ReactiveFormsModule, MatCardModule, MatListModule, RouterModule],
   declarations: [AuditsComponent, AddAuditComponent],
   exports: [AuditsComponent],
 })
